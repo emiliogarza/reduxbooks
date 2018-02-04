d600e828d26a8a6ee5f63786c231c13f2a614d65
Emilio Garza <emiliogarza07@gmail.com>
2018-02-03 17:49:11 -0800

    Finalizing changes to the action and applying them to a new container

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
     return [
-        { title: "Javascript: The Good Parts" },
-        { title: "Harry Potter" },
-        { title: "The Pragmatic Programmer" },
-        { title: "Make Today Count" }
+        { title: "Javascript: The Good Parts", pages: 110 },
+        { title: "Harry Potter", pages: 85 },
+        { title: "The Pragmatic Programmer", pages: 85 },
+        { title: "Make Today Count", pages: 1 }
     ]
 }
